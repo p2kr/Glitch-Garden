f31e30998d4bdc413db490f3532528c464a18e89
Prince Kumar <34896515+prince2000kr@users.noreply.github.com>
2020-10-14 21:42:48 +0530

    Before ANimation

---
+++ Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour
+{
+    private int currentSceneIndex;
+    [SerializeField] private float timeToWait = 3f;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+}
