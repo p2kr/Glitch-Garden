a59430ff4a8443d54fa1cf94d120fc1390476a5f
Prince Kumar <34896515+prince2000kr@users.noreply.github.com>
2020-10-14 23:51:11 +0530

    Setup Defender prefab

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -2,16 +2,16 @@
 
 public class Attacker : MonoBehaviour
 {
-    [Range(0f, 5f)] [SerializeField] private float walkSpeed = 1f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    [Range(0f, 5f)] float currentSpeed = 1f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+        transform.Translate(Vector2.left * (currentSpeed * Time.deltaTime));
+    }
+
+    public void SetMovementSpeed(float speed)
+    {
+        currentSpeed = speed;
     }
 }
