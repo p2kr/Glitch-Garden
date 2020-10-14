eb77b6f4319388a693583213557573c5c2cd7708
Prince Kumar <34896515+prince2000kr@users.noreply.github.com>
2020-10-14 23:25:11 +0530

    Spawn Attackers using coroutines

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range(0f, 5f)] [SerializeField] private float walkSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] private float minSpawnDelay = 1f;
+    [SerializeField] private float maxSpawnDelay = 5f;
+    [SerializeField] private Attacker attackerPrefab;
+
+    private bool spawn = true;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
